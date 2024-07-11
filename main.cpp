04dd1923c4c74e695539121e15fe839ab1fd1d5b
Zedmous <zedmous@gmail.com>
2024-07-10 22:38:30 -0400

    comentario, ejercicio solucion del primer taller

--- main.cpp
+++ main.cpp
@@ -99,24 +99,61 @@ float calcularSuma(float a,float b){
 int main(int argc, char** argv) {
 
 	imprimirMensaje("----------INICIANDO PROGRAMA----------");
-	imprimirMensaje("--------------------------------------");
-	float numero1=0.0;
-	float numero2=0.0;
-	imprimirMensaje("----------PASO 1----------");
-	imprimirMensaje("----------LECTURA DE DATOS----------");
-	numero1=leerFloat("Ingrese el numero 1: ");
-	numero2=leerFloat("Ingrese el numero 2: ");
-	imprimirMensaje("----------PASO 2----------");
-	imprimirMensaje("----------PROCESANDO DATOS----------");
-	float suma=calcularSuma(numero1,numero2);
-	imprimirMensaje("----------PASO 3----------");
-	imprimirMensaje("----------SALIDAS DE DATOS----------");
-	imprimirFloat("La suma es: ",suma);
-
-
-
-	imprimirMensaje("----------FIN DEL PROGRAMA----------");
-	imprimirMensaje("--------------------------------------");
+	//declaracion de variables
+	string razon_social="";
+	string rif_cedula="";
+	int resp=1,cont_mayor=0,cont_detal=0;
+	while(resp==1){
+		rif_cedula=leerString("Ingrese el rif o cedula: ");
+		razon_social=leerString("Ingrese la razon social: ");
+		int comprar=leerEntero("¿Que desea comprar? : Refrescos:1 , Maltas:2, Ambos:3  ");
+		int cantidad_r=0,cantidad_m=0;
+		float monto_pagar=0.0;
+		if(comprar==1){
+			cantidad_r=leerEntero("Ingrese la cantidad de refrescos a comprar: ");
+			if(cantidad_r>=6){
+				monto_pagar=5*cantidad_r;
+				cont_mayor++;//cont_mayor=cont_mayor+1;
+			}else{
+				monto_pagar=10*cantidad_r;
+				cont_detal++;
+			}
+			imprimirFloat("El monto a pagar para este cliente es de: ",monto_pagar);
+		}else if(comprar==2){
+			
+			cantidad_m=leerEntero("Ingrese la cantidad de maltas a comprar: ");
+			if(cantidad_m>=6){
+				monto_pagar=7*cantidad_m;
+				cont_mayor++;
+			}else{
+				monto_pagar=12*cantidad_m;
+				cont_detal++;//
+			}
+			imprimirFloat("El monto a pagar para este cliente es de: ",monto_pagar);
+		}else{
+			cantidad_r=leerEntero("Ingrese la cantidad de refrescos a comprar: ");
+			if(cantidad_r>=6){
+				monto_pagar=5*cantidad_r;
+				cont_mayor++;
+			}else{
+				monto_pagar=10*cantidad_r;
+				cont_detal++;
+			}
+			cantidad_m=leerEntero("Ingrese la cantidad de maltas a comprar: ");
+			if(cantidad_m>=6){
+				monto_pagar+=7*cantidad_m;//monto_pagar=monto_pagar+7*cantidad_m
+				cont_mayor++;
+			}else{
+				monto_pagar+=12*cantidad_m;
+				cont_detal++;
+			}
+			imprimirFloat("El monto a pagar para este cliente es de: ",monto_pagar);
+		}
+		resp=leerEntero("¿Otro cliente desea realizar una compra: si:1 , no:otro caso ");
+	}
+	imprimirMensaje("----------DATOS ESTADISTICOS----------");
+	imprimirEntero("La cantidad de ventas al mayor fueron de: ",cont_mayor);
+	imprimirEntero("La cantidad de ventas al detal fueron de: ",cont_detal);
 	return 0;
 }
 
